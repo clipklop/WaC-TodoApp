4d96b2fc030b92d6e6c64866971a75cd04379f19
clipklop <siliconchik@gmail.com>
2019-01-31 23:19:54 +0300

    Added  DOM buttons and inputs for iterraction

--- Todo App/todo_app.js
+++ Todo App/todo_app.js
@@ -25,14 +25,20 @@ const todoList = {
     }
   },
   changeTodo(position, todoText) {
+    if (typeof this.todos[position] === 'undefined') { return }
+
     this.todos[position].todoText = todoText;
     this.displayTodos();
   },
   deleteTodo(position) {
+    if (typeof this.todos[position] === 'undefined') { return }
+    
     this.todos.splice(position, 1);
     this.displayTodos();
   },
   toggleCompleted(position) {
+    if (typeof this.todos[position] === 'undefined') { return }
+
     let todo = this.todos[position];
     todo.completed = !todo.completed;
     this.displayTodos();
@@ -61,12 +67,61 @@ const todoList = {
 };
 
 
-// Browser scripting with DOM manipulation
+// Toggle ToDo's
 const displayButton = document.getElementById('displayButton');
 displayButton.addEventListener('click', (e) => todoList.displayTodos());
-
 const toggleAllButton = document.getElementById('toggleAllButton');
 toggleAllButton.addEventListener('click', (e) => todoList.toggleAll());
+
+// Add ToDo
+const addTodoButton = document.getElementById('addTodoButton');
+const addTodoInput = document.getElementById('addTodoInput');
+const addTodo = () => {
+  if (addTodoInput.value === '') return;
+  
+  todoList.addTodo(addTodoInput.value);
+
+  addTodoInput.value = '';
+}
+addTodoButton.addEventListener('click', addTodo);
+
+// Change ToDo
+const changeTodoButton = document.getElementById('changeTodoButton');
+const changeTodoPosition = document.getElementById('changeTodoPosition');
+const changeTodoInput = document.getElementById('changeTodoInput');
+const changeTodo = () => {
+  if (changeTodoInput.value === '' || changeTodoPosition.value === '') return;
+  
+  todoList.changeTodo(changeTodoPosition.valueAsNumber, changeTodoInput.value);
+
+  changeTodoInput.value = changeTodoPosition.value = '';
+}
+changeTodoButton.addEventListener('click', changeTodo);
+
+// Delete ToDo
+const deleteTodoButton = document.getElementById('deleteTodoButton');
+const deleteTodoPosition = document.getElementById('deleteTodoPosition');
+const deleteTodo = () => {
+  if (deleteTodoPosition.value === '') { return };
+  
+  todoList.deleteTodo(deleteTodoPosition.valueAsNumber);
+
+  deleteTodoPosition.value = '';
+}
+deleteTodoButton.addEventListener('click', deleteTodo);
+
+// Toggle one ToDo
+const toggleTodoButton = document.getElementById('toggleTodoButton');
+const toggleTodoPosition = document.getElementById('toggleTodoPosition');
+const toggleTodo = () => {
+  if (toggleTodoPosition.value === '') { return };
+  
+  todoList.toggleCompleted(toggleTodoPosition.valueAsNumber);
+
+  toggleTodoPosition.value = '';
+}
+toggleTodoButton.addEventListener('click', toggleTodo);
+
 
 // Module exporting for NodeJS
 // module.exports = todoList;
